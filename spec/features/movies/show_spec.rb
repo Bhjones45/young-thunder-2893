eb5a26ed21abc01a9fec935f040f64c02d382ddb
Brett Jones <bhjones4545@gmail.com>
2021-07-19 10:00:30 -0600

    Create test for movie and attributes

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -1,1 +1,12 @@
-require
+require 'rails_helper'
+
+RSpec.describe 'movie show page' do
+  it 'can show list of all moveis with attributes' do
+      studio = Studio.create!(name: 'Warner Bros.', location: "Burbank, California")
+      movie = studio.movies.create!(title: 'Matrix', creation_year: '1999', genre: 'Science Fiction')
+
+      visit "/movies/#{movie.id}"
+      
+      expect(page).to have_content('Matrix')
+    end
+end
