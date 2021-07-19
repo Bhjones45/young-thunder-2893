59c7adb9fa2baff2cfaee1fe30df2015110a62ce
Brett Jones <bhjones4545@gmail.com>
2021-07-19 10:46:50 -0600

    Create average age class method test

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe Actor do
     it { should have_many(:actor_movies) }
     it { should have_many(:movies).through(:actor_movies)}
   end
+
+  describe '::average_age_actors' do
+    it 'returns average age of all actors in a movie' do
+      studio = Studio.create!(name: 'Warner Bros.', location: "Burbank, California")
+      movie = studio.movies.create!(title: 'Matrix', creation_year: '1999', genre: 'Science Fiction')
+      actor_1 = Actor.create!(name: "Keanu", age: "28")
+      actor_2 = Actor.create!(name: "Carrie-Anne", age: "29")
+      actor_3 = Actor.create!(name: "Laurence Fishburne", age: "42")
+
+      expect(Actor.average_age_actors).to eq(33)
+    end
+  end
 end
